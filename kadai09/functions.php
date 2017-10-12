de5a0c8f6576f6af66f9adc4de26d351f10f8053
shoji\takuro <teru.two@gmail.com>
2017-10-13 04:21:00 +0900

    update functions.php

--- kadai09/functions.php
+++ kadai09/functions.php
@@ -35,6 +35,9 @@ if(!isset($_SESSION["chk_ssid"]) ||
   $_SESSION["chk_ssid"]!=session_id()){
     echo "Login Error.";
     exit();
+}else{
+    session_regenerate_id(true);
+    $_SESSION["chk_ssid"] = session_id();
 }
 }
 
